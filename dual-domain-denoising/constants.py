206659464286fa8c0a550aa2dac24cefa3f92771
Ying Yang <68610185+y-ng@users.noreply.github.com>
2025-03-14 15:11:01 -0400

    load images for training

--- dual-domain-denoising/constants.py
+++ dual-domain-denoising/constants.py
@@ -1,12 +1,17 @@
 # path for training and output data
 TRAIN_FOLDER = './dual-domain-denoising/training_data'
-OUTPUT_FOLDER = './dual-domain-denoising/training_data'
+OUTPUT_FOLDER = './dual-domain-denoising/outputs'
 
 # paths for noisy/clean kspace/image data
 NOISY_KDATA_PATH = TRAIN_FOLDER + '/noisy_train_kdata.pkl'
 CLEAN_KDATA_PATH = TRAIN_FOLDER + '/clean_train_kdata.pkl'
 NOISY_IMAGE_PATH = TRAIN_FOLDER + '/noisy_train_image.pkl'
 CLEAN_IMAGE_PATH = TRAIN_FOLDER + '/clean_train_image.pkl'
+
+# path for model outputs
+U_K_MODEL_PATH = OUTPUT_FOLDER + '/u_k_net.pt'
+U_I_MODEL_PATH = OUTPUT_FOLDER + '/u_i_net.pt'
+MODEL_LOSS_PATH = OUTPUT_FOLDER + '/model_loss.pkl'
 
 # seed for random state
 SEED = 42
--- dual-domain-denoising/training.py
+++ dual-domain-denoising/training.py
@@ -3,7 +3,7 @@ import pickle as pk
 import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, TensorDataset
 import fastmri
 from fastmri.data import transforms
 from constants import *
@@ -33,8 +33,7 @@ def main():
         clean_kdata = pk.load(handle)
         print(clean_kdata.dtype, clean_kdata.shape)
 
-    kdata_train_loader = DataLoader(torch.tensor(noisy_kdata), torch.tensor(clean_kdata))
-
+    """
     with open(NOISY_IMAGE_PATH, 'rb') as handle:
         noisy_image = pk.load(handle)
         print(noisy_image.dtype, noisy_image.shape)
@@ -42,18 +41,72 @@ def main():
     with open(CLEAN_IMAGE_PATH, 'rb') as handle:
         clean_image = pk.load(handle)
         print(clean_image.dtype, clean_image.shape)
+    """
 
-    image_train_loader = DataLoader(torch.tensor(noisy_image), torch.tensor(clean_image))
+    (n_samples, height, width) = noisy_kdata.shape
+    print(f'Number of training samples: {n_samples}')
 
+    # set up model
     u_k_net = UNet_kdata()
     u_i_net = UNet_image()
 
-    # 25 total epochs
     batch_size = 4
+
+    noisy_kdata_real = torch.tensor(noisy_kdata).real
+    noisy_kdata_imag = torch.tensor(noisy_kdata).imag
+
+    clean_kdata_real = torch.tensor(clean_kdata).real
+    clean_kdata_imag = torch.tensor(clean_kdata).imag
+
+    kdata_train_data = TensorDataset(
+        torch.stack([noisy_kdata_real.to(torch.float32), noisy_kdata_imag.to(torch.float32)], dim=1), 
+        torch.stack([clean_kdata_real.to(torch.float32), clean_kdata_imag.to(torch.float32)], dim=1)
+    )
+    kdata_train_loader = DataLoader(kdata_train_data, batch_size=batch_size, shuffle=True)
+
+    num_iters = 1 # num_batches = num_iters * 5
+
     u_k_optimizer = torch.optim.Adam(u_k_net.parameters(), lr=1e-4)
     u_i_optimizer = torch.optim.Adam(u_i_net.parameters(), lr=1e-4)
     u_k_criterion = nn.HuberLoss()
     u_i_criterion = nn.HuberLoss()
+
+    # train models
+    # TODO
+    for iter in range(num_iters):
+        print(f'Iteration {iter + 1}/{num_iters}')
+        
+        for i in range(5):
+            print(f'Epoch {(i + 1) * (iter + 1)}/{num_iters * 5} for U_k')
+            for noisy, clean in kdata_train_loader:
+                u_k_net_outputs = u_k_net(noisy)
+                u_k_net_loss = u_k_criterion(u_k_net_outputs, clean)
+
+                # update weights
+                u_k_optimizer.zero_grad()
+                u_k_net_loss.backward()
+                u_k_optimizer.step()
+
+
+    # save final models
+    print('Saving models and training outputs...')
+    torch.save(u_k_net.state_dict(), U_K_MODEL_PATH)
+    torch.save(u_i_net.state_dict(), U_I_MODEL_PATH)
+    
+
+    """
+    # load saved pytorch models for inference
+    print('Loading models...')
+    u_k_net_load = UNet_kdata()
+    u_k_net_load.load_state_dict(torch.load(U_K_MODEL_PATH, weights_only=True))
+    u_k_net_load.eval()
+
+    u_i_net_load = UNet_image()
+    u_i_net_load.load_state_dict(torch.load(U_I_MODEL_PATH, weights_only=True))
+    u_i_net_load.eval()
+
+    print('Done.')"
+    """
 
 
 if __name__ == '__main__':
